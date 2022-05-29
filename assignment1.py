aca7e8b95415dd9ac118c587e59b40269e6c72d0
arshagokulam <106462003+arshagokulam@users.noreply.github.com>
2022-05-29 11:41:12 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+
+# In[2]:
+
+
+# * :- Mathematial operator for mjultiplication
+# 'hello' :- String
+# -87.8 :- Float
+# - :- mathemaitical operator for subtraction
+# / :- mathematical operator for division
+# + :- mathematical operator for addition
+# 6 :- Integer
+
+
+# 2. What is the difference between string and variable?
+
+# In[4]:
+
+
+# String is a collection of alpha-numeric characters which is stored in a variable
+# Variable is a name given to the memory location which stores the values
+
+
+# 3. Describe three different data types.
+
+# In[6]:
+
+
+#Integers – It contains positive or negative whole numbers (without fraction or decimal).
+#Float – It is a real number with floating point representation. It is specified by a decimal point. Optionally, the character e or E followed by a positive or negative integer may be appended to specify scientific notation.
+#String – A string is a collection of one or more characters put in a single quote, double-quote or triple quote.
+
+
+# 4. What is an expression made up of? What do all expressions do?
+
+# In[8]:
+
+
+#An expression is a construct made up of variables, operators, and method invocations, which are constructed according to the syntax of the language, that evaluates to a single value.
+
+
+# 5. This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement?
+
+# In[10]:
+
+
+#Statements represent an action or command e.g print statements, assignment statements. 
+#Expression is a combination of variables, operations and values that yields a result value.
+#here , spam =10 means spam is a variable which has a variable 10, this is assignment statement
+# when coming to expression it checks whether the spam is equal to 10.
+
+
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+
+# In[13]:
+
+
+bacon=22
+
+
+# In[15]:
+
+
+bacon+1
+
+
+# 7. What should the values of the following two terms be?
+# &#39;spam&#39; + &#39;spamspam&#39;
+# &#39;spam&#39; * 3
+
+# In[18]:
+
+
+'spam' + 'spamspam'
+
+
+# In[20]:
+
+
+'spam' * 3
+
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+
+# In[21]:
+
+
+#Because, Variable names cannot begin with a number.
+
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+
+# In[22]:
+
+
+# int() , float() , and str( )
+
+
+# 10. Why does this expression cause an error? How can you fix it?
+# &#39;I have eaten &#39; + 99 + &#39; burritos.&#39;
+
+# In[24]:
+
+
+'I have eaten ' + 99 + ' burritos.'
+
+
+# In[25]:
+
+
+'I have eaten ' + '99' + ' burritos.'
+
